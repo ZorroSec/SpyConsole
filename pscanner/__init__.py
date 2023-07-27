2802e7eee9f067d83db09975e1ed9bf06e04ec9b
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-27 09:53:53 -0300

    limpando a tela

--- pscanner/__init__.py
+++ pscanner/__init__.py
@@ -4,3 +4,11 @@ import subprocess
 import platform
 import os
 from datetime import datetime
+
+def pscanner():
+	if platform.system() == "Windows":
+		subprocess.call('cls', shell=True)
+	else:
+		subprocess.call('clear', shell=True)
+
+	remoteServer = input(f"")
